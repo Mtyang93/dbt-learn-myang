350009c0172d807a6597beb9bb1dcc636288f6cb
Mtyang93 <43153754+Mtyang93@users.noreply.github.com>
2020-06-11 20:07:23 -0400

    dbt

---
+++ jaffle-shop/models/example/learn_stripe/ticket_tailor_charges.sql
@@ -0,0 +1,29 @@
+
+with source as (
+
+    select * from {{ source('learn_stripe', 'charges') }}
+
+),
+
+renamed as (
+
+    select
+        id,
+        captured,
+        created,
+        currency,
+        description,
+        failure_code,
+        failure_message,
+        fraud_details,
+        livemode,
+        object,
+        paid,
+        refunded,
+        status
+
+    from source
+
+)
+
+select * from renamed
